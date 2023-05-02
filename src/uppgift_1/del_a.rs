4dad2a55fa25cb4235dbeb9dd75a8e6a8270f542
u296 <=>
2023-05-02 23:18:19 +0200

    w

---
+++ src/uppgift_1/del_a.rs
@@ -0,0 +1,16 @@
+use super::prelude::*;
+
+pub async fn uppgift_a() {
+    ensure_dir_exists("uppgifter/1/a").await;
+
+    let mut output_file = File::create(&format!("uppgifter/1/a/results.csv"))
+        .await
+        .unwrap();
+    uppgift1_run_simulation(
+        *super::BALL_SNAPSHOT,
+        *super::BALL_AIR_RESISTANCE,
+        0.001,
+        &mut output_file,
+    )
+    .await;
+}
